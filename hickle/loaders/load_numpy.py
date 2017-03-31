c80f4256dc69ad1b0fabb86849367e9b0ff23fe8
Danny Price <dan@thetelegraphic.com>
2017-03-31 16:10:16 +0800

    Moved sparse_matrix loaders to only load if scipy is installed

--- hickle/loaders/load_numpy.py
+++ hickle/loaders/load_numpy.py
@@ -33,23 +33,6 @@ def check_is_numpy_array(py_obj):
     return is_numpy
 
 
-def check_is_scipy_sparse_array(py_obj):
-    """ Check if a python object is a scipy sparse array
-
-    Args:
-        py_obj: python object to check whether it is a sparse array
-
-    Returns
-        is_numpy (bool): Returns True if it is a sparse array, else False if it isn't
-    """
-    t_csr = type(scipy.sparse.csr_matrix([0]))
-    t_csc = type(scipy.sparse.csc_matrix([0]))
-    t_bsr = type(scipy.sparse.bsr_matrix([0]))
-    is_sparse = type(py_obj) in (t_csr, t_csc, t_bsr)
-
-    return is_sparse
-
-
 def create_np_scalar_dataset(py_obj, h_group, call_id=0, **kwargs):
     """ dumps an np dtype object to h5py file
 
@@ -97,34 +80,6 @@ def create_np_array_dataset(py_obj, h_group, call_id=0, **kwargs):
     else:
         d = h_group.create_dataset('data_%i' % call_id, data=py_obj, **kwargs)
         d.attrs["type"] = [b'ndarray']
-
-
-def create_sparse_dataset(py_obj, h_group, call_id=0, **kwargs):
-    """ dumps an sparse array to h5py file
-
-    Args:
-        py_obj: python object to dump; should be a numpy array or np.ma.array (masked)
-        h_group (h5.File.group): group to dump data into.
-        call_id (int): index to identify object's relative location in the iterable.
-    """
-    h_sparsegroup = h_group.create_group('data_%i' % call_id)
-    data    = h_sparsegroup.create_dataset('data',    data=py_obj.data, **kwargs)
-    indices = h_sparsegroup.create_dataset('indices', data=py_obj.indices, **kwargs)
-    indptr  = h_sparsegroup.create_dataset('indptr',  data=py_obj.indptr, **kwargs)
-    shape   = h_sparsegroup.create_dataset('shape',   data=py_obj.shape, **kwargs)
-
-    if isinstance(py_obj, type(sparse.csr_matrix([0]))):
-        type_str = b'csr'
-    elif isinstance(py_obj, type(sparse.csc_matrix([0]))):
-        type_str = b'csc'
-    elif isinstance(py_obj, type(sparse.bsr_matrix([0]))):
-        type_str = b'bsr'
-
-    h_sparsegroup.attrs["type"] = [b'%s_matrix' % type_str]
-    data.attrs["type"] = [b"%s_matrix_data" % type_str]
-    indices.attrs["type"] = [b"%s_matrix_indices" % type_str]
-    indptr.attrs["type"] = [b"%s_matrix_indptr" % type_str]
-    shape.attrs["type"] = [b"%s_matrix_shape" % type_str]
 
 
 
@@ -197,6 +152,50 @@ hkl_types_dict = {
 ###########
 
 if _has_scipy:
+
+    def check_is_scipy_sparse_array(py_obj):
+        """ Check if a python object is a scipy sparse array
+
+        Args:
+            py_obj: python object to check whether it is a sparse array
+
+        Returns
+            is_numpy (bool): Returns True if it is a sparse array, else False if it isn't
+        """
+        t_csr = type(scipy.sparse.csr_matrix([0]))
+        t_csc = type(scipy.sparse.csc_matrix([0]))
+        t_bsr = type(scipy.sparse.bsr_matrix([0]))
+        is_sparse = type(py_obj) in (t_csr, t_csc, t_bsr)
+
+        return is_sparse
+
+
+    def create_sparse_dataset(py_obj, h_group, call_id=0, **kwargs):
+        """ dumps an sparse array to h5py file
+
+        Args:
+            py_obj: python object to dump; should be a numpy array or np.ma.array (masked)
+            h_group (h5.File.group): group to dump data into.
+            call_id (int): index to identify object's relative location in the iterable.
+        """
+        h_sparsegroup = h_group.create_group('data_%i' % call_id)
+        data = h_sparsegroup.create_dataset('data', data=py_obj.data, **kwargs)
+        indices = h_sparsegroup.create_dataset('indices', data=py_obj.indices, **kwargs)
+        indptr = h_sparsegroup.create_dataset('indptr', data=py_obj.indptr, **kwargs)
+        shape = h_sparsegroup.create_dataset('shape', data=py_obj.shape, **kwargs)
+
+        if isinstance(py_obj, type(sparse.csr_matrix([0]))):
+            type_str = b'csr'
+        elif isinstance(py_obj, type(sparse.csc_matrix([0]))):
+            type_str = b'csc'
+        elif isinstance(py_obj, type(sparse.bsr_matrix([0]))):
+            type_str = b'bsr'
+
+        h_sparsegroup.attrs["type"] = [b'%s_matrix' % type_str]
+        data.attrs["type"] = [b"%s_matrix_data" % type_str]
+        indices.attrs["type"] = [b"%s_matrix_indices" % type_str]
+        indptr.attrs["type"] = [b"%s_matrix_indptr" % type_str]
+        shape.attrs["type"] = [b"%s_matrix_shape" % type_str]
 
     def load_sparse_matrix_data(h_node):
         py_type, data = get_type_and_data(h_node)
