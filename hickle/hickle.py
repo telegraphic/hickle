50b16fcc7cf72ee992bbb6cc598fada396716146
Danny Price <dan@thetelegraphic.com>
2020-05-20 09:46:44 +1000

    Update file_opener re #123

--- hickle/hickle.py
+++ hickle/hickle.py
@@ -174,7 +174,7 @@ def file_opener(f, mode='r', track_times=True):
     close_flag = True
 
     # Were we handed a file object or just a file name string?
-    if isinstance(f, (file, io.TextIOWrapper)):
+    if isinstance(f, (file, io.TextIOWrapper, io.BufferedWriter)):
         filename, mode = f.name, f.mode
         f.close()
         h5f = h5.File(filename, mode)
